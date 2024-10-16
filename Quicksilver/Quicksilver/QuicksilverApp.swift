a51e3874603293c02576dc14e00f02660bde2030
Phillip <78912067+lebrozaurjagger@users.noreply.github.com>
2024-10-16 14:03:10 +0300

    1.0.1

---
+++ Quicksilver/Quicksilver/QuicksilverApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuicksilverApp.swift
+//  Quicksilver
+//
+//  Created by Phillip on 16.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct QuicksilverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Quicksilver/macOS/Views/MainScreenView.swift
@@ -0,0 +1,47 @@
+//
+//  MainScreenView.swift
+//  Quicksilver
+//
+//  Created by Phillip on 16.10.2024.
+//
+
+import SwiftUI
+
+struct MainScreenView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8)
+                        .frame(width: 128, height: 64)
+                        .foregroundColor(Color(.quaternarySystemFill))
+                    
+                    VStack {
+                        ZStack {
+                            Text("Quicksilver")
+                                .font(.custom("Qwitcher Grypen Regular", size: 32)).foregroundStyle(
+                                    LinearGradient(gradient: Gradient(colors: [Color(.systemGray), Color(.darkGray)]),
+                                                   startPoint: .leading,
+                                                   endPoint: .trailing)
+                                )
+                            
+                            Text("macOS")
+                                .font(.system(size: 8))
+                                .offset(x: 44, y: -8)
+                        }
+                        
+                        Text("by Phillip Samedi")
+                    }
+                }
+                .padding()
+                
+                Spacer()
+            }
+        }
+        .frame(width: 400, height: 200)
+    }
+}
+
+#Preview {
+    MainScreenView()
+}
